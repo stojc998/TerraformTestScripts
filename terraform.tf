babdc60a6ed66ddb38023e10bc043a57cf4aa5ec
Blaz <stojan.blaz@gmail.com>
2023-06-19 20:14:09 +0200

    Moved terraform.tf file to root. Added s3 bucket for terraform.tfstate file.

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,21 @@
 terraform {
+
+  #this is done by default in  Terraform
+  /*backend "local" {
+    path = "terraform.tfstate"
+  }//backend "local"*/
+
+  #we store our state file in a s3 bucket. It's going to create prod in in that a file prod/aws_infra
+  backend "s3" {
+    bucket = TERRAFORM_LOCK_BUCKET #using exports
+    key = TERRAFORM_LOCK_KEY
+    region = "us-east-1a"
+
+    #locking mechanism
+    dynamodb_table = TERRAFORM_DYNAMO_TABLE
+    encrypt = true
+  }//backend "local"
+
   required_version = ">=1.4.0"
   #required_version = "=1.5.0"//mora bit taksna verzija
   #required_version = "~>1.5.0"//tilda. pomeni da je najbol desna stevilka je lahk spremenljiva
